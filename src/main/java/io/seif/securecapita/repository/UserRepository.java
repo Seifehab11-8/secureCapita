001ce92f37499fa48e626b9d0da812f693d3bb3e
Seifehab11-8 <semaziz2004@yahoo.com>
2026-03-13 17:13:56 +0200

    feat: simplify UserRepository by removing redundant CRUD method overrides

--- src/main/java/io/seif/securecapita/repository/UserRepository.java
+++ src/main/java/io/seif/securecapita/repository/UserRepository.java
@@ -1,21 +1,9 @@
 package io.seif.securecapita.repository;
 
 import io.seif.securecapita.model.User;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    /* Simple Crud Operations */
-    @Override
-    Page<User> findAll(Pageable pageable);
-
-    @Override
-    <S extends User> S save(S entity);
-
-    User getUserByUserId(Integer userId);
-
-    boolean deleteUserByUserId(Integer userId);
 
 }
